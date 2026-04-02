78464344e03739da6963788518da07b51b276c92
sonu-kumar2085 <sonukumar20082005@gmail.com>
2026-04-03 00:39:01 +0530

    add create stock feature for admin

--- Backend/src/routes/stock.routes.js
+++ Backend/src/routes/stock.routes.js
@@ -8,5 +8,5 @@ router.route("/").get(getAllStocks)
 router.route("/portfolio").get(verifyJWT, getUserStocks)
 router.route("/order").post(verifyJWT, placeOrder)
 router.route("/orders").get(verifyJWT, getMyOrders)
-router.route("/createstock").get(createStock)
+router.route("/createstock").post(createStock)
 export default router
